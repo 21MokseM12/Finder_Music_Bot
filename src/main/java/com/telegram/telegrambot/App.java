a91dd40971129789a33081d18346f39c603866c6
21MokseM12 <maksim.buynov.00@mail.ru>
2024-07-27 21:14:56 +0300

    refactor: add description to application.properties file; delete unit tests

--- src/main/java/com/telegram/telegrambot/App.java
+++ src/main/java/com/telegram/telegrambot/App.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TelegrammBotApplication {
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(TelegrammBotApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
-
 }
--- src/main/java/com/telegram/telegrambot/config/BotInitializer.java
+++ src/main/java/com/telegram/telegrambot/config/BotInitializer.java
@@ -1,6 +1,6 @@
 package com.telegram.telegrambot.config;
 
-import com.telegram.telegrambot.service.TelegramBot;
+import com.telegram.telegrambot.services.TelegramBot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
